f6ee395cdb5b17b08d1e262f928f46588f6b34e0
Ashu Kher <ashukher1@gmail.com>
2020-05-08 10:53:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ def parse_keys(file_obj):
 
     for line in file_obj: 
         line_list = line.split(":")
-        keys_dict[line_list[0]] = line_list[1].strip("\n")
+        keys_dict[line_list[0]] = line_list[1].strip("\n").strip(" ").strip("\t")
 
     return keys_dict
 
 
 def main(): 
+
+    # use the template for to create api_keys.txt
     file_obj = open("api_keys.txt", "r")
     keys_dict = parse_keys(file_obj)
 
+    # setting this up for a paper trading account 
     url = "https://paper-api.alpaca.markets"
 
     api = trade_api.REST(keys_dict["key"], keys_dict["secret"], url, api_version="v2")
