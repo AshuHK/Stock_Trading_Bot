b7f94dc869ffc8f8a38388b8f68343c72ef221c6
Ashu Kher <ashukher1@gmail.com>
2020-05-08 10:40:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+import alpaca_trade_api as trade_api 
+import time
 
 def parse_keys(file_obj): 
     keys_dict = {} 
-    for line in file_obj: 
-        # print(line)
-        line_list = line.split(":")
-        
-        keys_dict[line_list[0]] = line_list[1]
 
-        # print(line_list)
+    for line in file_obj: 
+        line_list = line.split(":")
+        keys_dict[line_list[0]] = line_list[1].strip("\n")
 
     return keys_dict
 
 
 def main(): 
     file_obj = open("api_keys.txt", "r")
-    
     keys_dict = parse_keys(file_obj)
-    
-    print(keys_dict)
+
+    url = "https://paper-api.alpaca.markets"
+
+    api = trade_api.REST(keys_dict["key"], keys_dict["secret"], url, api_version="v2")
+
+    account = api.get_account() 
+
+    print(account.status)
 
     pass
 
