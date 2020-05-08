e42f87d9148a38ff6d5842e90f0201c83eb8df3f
Ashu Kher <ashukher1@gmail.com>
2020-05-08 10:30:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 
 def parse_keys(file_obj): 
-
+    keys_dict = {} 
     for line in file_obj: 
-        print(line)
-    pass
+        # print(line)
+        line_list = line.split(":")
+        
+        keys_dict[line_list[0]] = line_list[1]
+
+        # print(line_list)
+
+    return keys_dict
 
 
 def main(): 
     file_obj = open("api_keys.txt", "r")
     
     keys_dict = parse_keys(file_obj)
+    
+    print(keys_dict)
 
     pass
 
